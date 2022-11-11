66121ef796e41ffe634e5167edfd72070ff64733
joewontae <tyuiop3762@naver.com>
2022-11-11 15:32:20 +0900

    Feat: 레이아웃 컴포넌트 구현

---
+++ client/src/pages/PagesLayout/Layout.jsx
@@ -0,0 +1,17 @@
+import React ,{ Suspense } from "react";
+import { Outlet } from "react-router-dom";
+import {Nav,Footer} from "../../components/@Layout"
+
+const Layout = () => {
+    return(
+        <>
+        <Nav />
+        <Suspense fallback={<Suspense/>}>
+            <Outlet/>
+        </Suspense>
+        <Footer/>
+        </>
+    )
+}
+
+export default Layout;
