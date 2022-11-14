121f13bbbfb2349901db8f039f20d3177536728e
js-ha <jshaha0911@gmail.com>
2022-11-14 23:57:45 +0900

    feat: book 컴포넌트 구현

---
+++ client/src/components/@Layout/Book/Book.style.js
@@ -0,0 +1,40 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  div {
+    text-align: center;
+  }
+`;
+
+const Title = styled.div`
+  display: flex;
+  font-weight: bold;
+  font-size: 25px;
+`;
+
+const Bookbox = styled.div`
+  padding: 20px;
+  display: grid;
+  grid-template-columns: repeat(5, minmax(0, auto));
+  max-width: 900px;
+  @media screen and (max-width: 810px) {
+    max-width: 100%;
+    grid-template-columns: repeat(4, minmax(0, auto));
+  }
+  @media screen and (max-width: 660px) {
+    max-width: 100%;
+    grid-template-columns: repeat(3, minmax(0, auto));
+  }
+  span {
+    margin-bottom: 30px;
+  }
+`;
+
+const Img = styled.img`
+  width: 150px;
+  margin: 5px;
+`;
+
+export { Container, Bookbox, Img, Title };
