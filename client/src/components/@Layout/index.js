37b1fc594cd64eddae00559400d877856883d8c4
rosenfence <rosenfence@gmail.com>
2022-11-14 21:18:53 +0900

    feat: create review modal

--- client/src/components/@Layout/index.js
+++ client/src/components/@Layout/index.js
@@ -1,4 +1,5 @@
-import Nav from "./Nav/Nav"
-import Footer from "./Footer/Footer"
+import Nav from './Nav/Nav';
+import Footer from './Footer/Footer';
+import ModalReview from '../Modal/Review';
 
-export {Nav, Footer};
+export { Nav, Footer, ModalReview };
