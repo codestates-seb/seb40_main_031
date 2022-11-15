c23f47155f230dbfb67eb99dd4cc29bc796178b4
nahoyeon <nahy0107@naver.com>
2022-11-15 09:28:15 +0900

    feat: Review 컴포넌트 구현

---
+++ client/src/components/@Layout/ReviewContent/ReviewSmall.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import * as S from './ReviewSmall.style'
+
+import {FaRegThumbsUp} from 'react-icons/fa'
+
+const ReviewSmall = () => {
+    return(
+        <>
+            <S.Template>
+                <S.Content>
+                    이 책은 내 앞의 대상에 대해 익숙하게 명명하고 
+                    분류함으로써 이해의 그물로 포획혀라는 인간의 높은 자리로부터 벗어나 세계를, 
+                    세계의 쉽사리 범주화 될 수 없는 모든 존재들을 물끄러미 그 자체로 바라보게 한다.
+                    이 책은 내 앞의 대상에 대해 익숙하게 명명하고 
+                    분류함으로써 이해의 그물로 포획혀라는 인간의 높은 자리로부터 벗어나 세계를, 
+                    세계의 쉽사리 범주화 될 수 없는 모든 존재들을 물끄러미 그 자체로 바라보게 한다.
+                    이 책은 내 앞의 대상에 대해 익숙하게 명명하고 
+                    분류함으로써 이해의 그물로 포획혀라는 인간의 높은 자리로부터 벗어나 세계를, 
+                    세계의 쉽사리 범주화 될 수 없는 모든 존재들을 물끄러미 그 자체로 바라보게 한다.
+                    이 책은 내 앞의 대상에 대해 익숙하게 명명하고 
+                    분류함으로써 이해의 그물로 포획혀라는 인간의 높은 자리로부터 벗어나 세계를, 
+                    세계의 쉽사리 범주화 될 수 없는 모든 존재들을 물끄러미 그 자체로 바라보게 한다.
+                </S.Content>
+                <S.BottomContent>
+                    <S.GoodIcon>
+                        <FaRegThumbsUp />
+                    </S.GoodIcon>&nbsp;5
+                </S.BottomContent>
+            </S.Template>
+        </>
+    )
+}
+export default ReviewSmall;
