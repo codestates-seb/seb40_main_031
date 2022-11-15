56f8309624c5bf549477c802f44863cb8dbccf10
rosenfence <rosenfence@gmail.com>
2022-11-15 09:50:21 +0900

    fix: Disable GlobalStyle-color

--- client/src/style/globalStyles.jsx
+++ client/src/style/globalStyles.jsx
@@ -3,7 +3,6 @@ import { createGlobalStyle } from 'styled-components';
 const GlobalStyles = createGlobalStyle`
 * {
   font-family: -apple-system,BlinkMacSystemFont,"Segoe UI Adjusted","Segoe UI","Liberation Sans",sans-serif;
-    color: black;
     margin: 0;
     padding: 0;
     box-sizing: border-box;
