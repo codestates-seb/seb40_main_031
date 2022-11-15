7d0e4f17cf1d0dcb61dafe036b109b93faf15092
rosenfence <90300215+rosenfence@users.noreply.github.com>
2022-11-15 21:29:18 +0900

    Fix: 파일 컨벤션, 루트 등 바로잡기 (#33)
    * Delete Cached file
    
    * fix: fixed file convention, route

--- client/src/components/@layout/nav/Nav.js
+++ client/src/components/@layout/nav/Nav.js
@@ -1,26 +1,26 @@
-import React from "react";
-import { FaBars,FaReadme } from "react-icons/fa"; 
+import React from 'react';
+import { FaBars, FaReadme } from 'react-icons/fa';
 
-import SearchBar from "../../@common/SearchBar/SearchBar";
-import SingUpButton from "../../SingUpButton/SingUpButton";
+import SearchBar from '../../@common/searchBar/SearchBar';
+import SingUpButton from '../../signUpButton/SignUpButton';
 
-import * as S from "./Nav.style"
+import * as S from './Nav.style';
 
-const Nav  = () =>{
-    return(
-        <S.Wrap>
-            <S.LayoutContainer>
-            <FaBars className="logo"/>
-            <FaReadme className="logo" style={{marginLeft:"240px"}}/>
-            
-            <S.Container>
-            <SearchBar/>
-            <SingUpButton title={"회원가입"}/>
-            <SingUpButton title={"로그인"} />
-            </S.Container>
-            </S.LayoutContainer>
-        </S.Wrap>
-    )
-}
+const Nav = () => {
+  return (
+    <S.Wrap>
+      <S.LayoutContainer>
+        <FaBars className='logo' />
+        <FaReadme className='logo' style={{ marginLeft: '240px' }} />
 
-export default Nav
+        <S.Container>
+          <SearchBar />
+          <SingUpButton title={'회원가입'} />
+          <SingUpButton title={'로그인'} />
+        </S.Container>
+      </S.LayoutContainer>
+    </S.Wrap>
+  );
+};
+
+export default Nav;
