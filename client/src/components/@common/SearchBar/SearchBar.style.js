37038cde4653562c7d9b39c41313add4153f9a1b
joewontae <tyuiop3762@naver.com>
2022-11-15 15:46:34 +0900

    ✒️Feat:검색창 컴포넌트 구현

---
+++ client/src/components/@common/SearchBar/SearchBar.style.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+
+const Wrap = styled.div`
+`
+
+const Input = styled.input`
+border:1px solid rgba(0,0,0,0.4);
+border-radius: 20px;
+padding:10px 10px 10px 45px;
+width: 15vw;
+`
+
+
+
+export {Wrap, Input}
