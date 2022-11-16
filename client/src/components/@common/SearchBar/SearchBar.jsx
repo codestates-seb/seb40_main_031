4e8cb49d9ddf97a3ac3448e1d209b62c6490f766
joewontae <tyuiop3762@naver.com>
2022-11-16 14:17:13 +0900

    🛠Fix:검색창 아이콘 버그수정

--- client/src/components/@common/SearchBar/SearchBar.jsx
+++ client/src/components/@common/SearchBar/SearchBar.jsx
@@ -1,15 +1,23 @@
-import React from "react";
-import * as S from "./SearchBar.style"
+import React from 'react';
+import { FaSistrix } from 'react-icons/fa';
+import * as S from './SearchBar.style';
 
-import {FaSistrix}  from "react-icons/fa";
-
-const SearchBar = () =>{
-    return(
+const SearchBar = () => {
+  return (
     <S.Wrap>
-        <FaSistrix className="logo" style={{fontSize:"2em",position:"absolute",top:30,right:770}}/>
-      <S.Input />
+      <S.SearchContainer>
+        <S.Input type='text' action='submit' name='searchbar' />
+        <FaSistrix
+          style={{
+            position: 'absolute',
+            top: '20%',
+            right: '88%',
+            fontSize: '1.9em',
+          }}
+        />
+      </S.SearchContainer>
     </S.Wrap>
-    )
-}
+  );
+};
 
-export default SearchBar
+export default SearchBar;
--- client/src/components/@common/SearchBar/SearchBar.style.js
+++ client/src/components/@common/SearchBar/SearchBar.style.js
@@ -1,24 +1,23 @@
-import styled from "styled-components";
-import { Theme } from "../../../style";
+import styled from 'styled-components';
+import { Theme } from '../../../style';
 
-
-const Wrap = styled.div`
-`
+const Wrap = styled.div``;
 
 const Input = styled.input`
-border:1px solid rgba(0,0,0,0.4);
-border-radius: 20px;
-padding:10px 10px 10px 45px;
-width: 15vw;
+  border: 1px solid rgba(0, 0, 0, 0.4);
+  border-radius: 20px;
+  padding: 10px 10px 10px 45px;
+  width: 15vw;
+  position: relative;
+  &:hover {
+    border: 1px solid ${Theme.PRIMARY.GREEN_DARK};
+  }
+  &:focus {
+    outline: rgba(0, 0, 0, 0.4);
+  }
+`;
 
-&:hover{
-    border:1px solid ${Theme.PRIMARY.GREEN_DARK}
-}
-&:focus{
-    outline: rgba(0,0,0,0.4);
-}
-`
-
-
-
-export {Wrap, Input}
+const SearchContainer = styled.div`
+  position: relative;
+`;
+export { Wrap, Input, SearchContainer };
