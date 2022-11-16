85bcc7e526831e63880269fdad7bf7f00fd0e085
js-ha <jshaha0911@gmail.com>
2022-11-17 03:03:06 +0900

    feat : 국내, 해외 서적 카테고리 리스트

---
+++ client/src/components/searchDetail/DomesticCategory.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import * as S from './Category.style';
+
+const DomesticCategory = () => {
+  return (
+    <S.ContainerDiv>
+      <S.FlexDiv className='domestic'>
+        <S.LinkA href='#'>소설</S.LinkA>
+        <S.LinkA href='#'>시/에세이</S.LinkA>
+        <S.LinkA href='#'>예술/대중문화</S.LinkA>
+        <S.LinkA href='#'>사회과학</S.LinkA>
+        <S.LinkA href='#'>역사와 문화</S.LinkA>
+        <S.LinkA href='#'>잡지</S.LinkA>
+        <S.LinkA href='#'>만화</S.LinkA>
+        <S.LinkA href='#'>유아</S.LinkA>
+        <S.LinkA href='#'>아동</S.LinkA>
+        <S.LinkA href='#'>가정과 생활</S.LinkA>
+        <S.LinkA href='#'>청소년</S.LinkA>
+        <S.LinkA href='#'>초등학습서</S.LinkA>
+        <S.LinkA href='#'>고등학습서</S.LinkA>
+        <S.LinkA href='#'>국어/외국어/사전</S.LinkA>
+        <S.LinkA href='#'>자연과 과학</S.LinkA>
+        <S.LinkA href='#'>경제경영</S.LinkA>
+        <S.LinkA href='#'>자기계발</S.LinkA>
+        <S.LinkA href='#'>인문</S.LinkA>
+        <S.LinkA href='#'>종교/역학</S.LinkA>
+        <S.LinkA href='#'>컴퓨터/인터넷</S.LinkA>
+        <S.LinkA href='#'>자격서/수험서</S.LinkA>
+        <S.LinkA href='#'>취미/레저</S.LinkA>
+        <S.LinkA href='#'>전공도서/대학교재</S.LinkA>
+        <S.LinkA href='#'>건강/뷰티</S.LinkA>
+        <S.LinkA href='#'>여행</S.LinkA>
+        <S.LinkA href='#'>중등학습서</S.LinkA>
+      </S.FlexDiv>
+    </S.ContainerDiv>
+  );
+};
+export default DomesticCategory;
