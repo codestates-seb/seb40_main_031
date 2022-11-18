644e75461bb11de137b1de74aac3f940eafe43a8
나호연 <91938347+nahoyeon@users.noreply.github.com>
2022-11-18 22:26:52 +0900

    feat : 책 상세 페이지 구현 , BookStore 컴포넌트 구현 , (ReviewBig, ReviewSmall) 컴포넌트 수정 (#45)
    * fix : 컴포넌트 수정 완료
    
    * fix : 컴포넌트 css 수정
    
    * feat : BookStore 컴포넌트 구현 완료
    
    * feat : 책 상세 페이지 구현 완료

--- client/src/components/@layout/reviewContent/ReviewBig.js
+++ client/src/components/@layout/reviewContent/ReviewBig.js
@@ -1,90 +1,86 @@
 import React from 'react';
 import * as S from './ReviewBig.style';
-import {FaRegUserCircle} from 'react-icons/fa';
-import {FaRegThumbsUp} from 'react-icons/fa'
-import {HiOutlinePencil} from 'react-icons/hi';
-import {MdClose} from 'react-icons/md';
+import { FaRegUserCircle } from 'react-icons/fa';
+import { FaRegThumbsUp } from 'react-icons/fa';
+import { HiOutlinePencil } from 'react-icons/hi';
+import { MdClose } from 'react-icons/md';
 import InfiniteScroll from 'react-infinite-scroll-component';
-import {useState} from 'react';
-import DummyReviewDetails from '../../../dummyData/DummyReviewDetail';
+import { useState } from 'react';
+import DummyReviewBigDetails from '../../../dummyData/DummyReviewBigDetail';
 import Loading from '../loading/Loading';
 
 // 전역변수 로 number 를 0으로 지정해주었다. 0으로 지정해준 이유는 맨 첫번째 usestate에서 실행된 (0,3)을 먼저
-// 실행했기 때문에(처음에 랜더링이 되어서) 밑에 있는 number += 3이 먼저 실행이 되어서 number가 3으로 바뀌게 
+// 실행했기 때문에(처음에 랜더링이 되어서) 밑에 있는 number += 3이 먼저 실행이 되어서 number가 3으로 바뀌게
 // 되고 그 상태에서 fetchdata를 불러주기 때문에 (3, 6) 증감연산자 때문에 (6,9) 순차적으로 3씩 증가.
 let number = 0;
 
 const ReviewBig = () => {
-    const [reviewDetails, setReviewDetails] = useState(DummyReviewDetails.slice(0,3));
-    const [hasMore, setHasMore] = useState(true)
-    // const [reviewDetails, setReviewDetails] = useState(Array.from({length:10}));
-    
-    const fetchData = () => {
-        if(reviewDetails.length < 30){
-            setTimeout(() => {
-                setReviewDetails(reviewDetails.concat(DummyReviewDetails.slice(number,number + 3)));
-                
-            }, 2000);
-        }else{
-            setHasMore(false);
-        }
-    };
-    number += 3;
+  const [reviewBigs, setreviewBigs] = useState(
+    DummyReviewBigDetails.slice(0, 3),
+  );
+  const [hasMore, setHasMore] = useState(true);
+  // const [reviewBigs, setreviewBigs] = useState(Array.from({length:10}));
 
-    return(
-        <div>
-            <InfiniteScroll
-                style = {{display: 'flex',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    flexDirection: 'column'}}
-                dataLength={reviewDetails.length}
-                next={fetchData}
-                hasMore={hasMore}
-                loader={<Loading width ="30px" height = "30px"/>}
-                endMessage={<h1 style = {{textAlign: 'center'}}>You are all set!</h1>}
-            >   
-            {reviewDetails.map((reviewDetail) => {
-                return(
-                <S.Template key =  {reviewDetail.id}>
-                    <S.UserInfo>
-                        <S.UserIcon>
-                            <FaRegUserCircle />
-                        </S.UserIcon>
-                        <S.UserName>
-                            {reviewDetail.userName}
-                        </S.UserName>
-                    </S.UserInfo>
-                    <S.Content>
-                        {reviewDetail.content}
-                    </S.Content>
-                    <S.BottomContent>
-                
-                        <S.LeftIconBox>
-                            <S.LeftIcon>
-                                <FaRegThumbsUp />
-                            </S.LeftIcon>
-                            <S.LeftText>
-                                {reviewDetail.vote}
-                            </S.LeftText>
-                        </S.LeftIconBox>
-                
-                        <S.RightIconBox>
-                            <S.RightIconUpdate>
-                                <HiOutlinePencil />
-                            </S.RightIconUpdate>
-                            <S.RightIconDelete>
-                                <MdClose />
-                            </S.RightIconDelete>
-                        </S.RightIconBox>
+  const fetchData = () => {
+    if (reviewBigs.length < 30) {
+      setTimeout(() => {
+        setreviewBigs(
+          reviewBigs.concat(DummyReviewBigDetails.slice(number, number + 3)),
+        );
+      }, 2000);
+    } else {
+      setHasMore(false);
+    }
+  };
+  number += 3;
 
-                    </S.BottomContent>
-                </S.Template>
-                
-                )
-            })}
-            </InfiniteScroll> 
-        </div>
-    )
-}
+  return (
+    <div>
+      <InfiniteScroll
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          flexDirection: 'column',
+        }}
+        dataLength={reviewBigs.length}
+        next={fetchData}
+        hasMore={hasMore}
+        loader={<Loading width='30px' height='30px' />}
+        endMessage={<h1 style={{ textAlign: 'center' }}>You are all set!</h1>}
+      >
+        {reviewBigs.map((reviewBig) => {
+          return (
+            <S.Template key={reviewBig.id}>
+              <S.UserInfo>
+                <S.UserIcon>
+                  <FaRegUserCircle />
+                </S.UserIcon>
+                <S.UserName>{reviewBig.userName}</S.UserName>
+              </S.UserInfo>
+              <S.Content>{reviewBig.content}</S.Content>
+              <S.BottomContent>
+                <S.LeftIconBox>
+                  <S.LeftIcon>
+                    <FaRegThumbsUp />
+                  </S.LeftIcon>
+                  <S.LeftText>{reviewBig.vote}</S.LeftText>
+                </S.LeftIconBox>
+
+                <S.RightIconBox>
+                  <S.RightIconUpdate>
+                    <HiOutlinePencil />
+                  </S.RightIconUpdate>
+                  <S.RightIconDelete>
+                    <MdClose />
+                  </S.RightIconDelete>
+                </S.RightIconBox>
+              </S.BottomContent>
+            </S.Template>
+          );
+        })}
+      </InfiniteScroll>
+    </div>
+  );
+};
 export default ReviewBig;
---
+++ client/src/pages/bookDetail/BookDetail.js
@@ -0,0 +1,73 @@
+import React from "react";
+import * as S from "./BookDetail.style";
+import Button from "../../components/@layout/button/Button";
+import DummyBookDetail from "../../dummyData/DummyBookDetail";
+import { HiOutlineChat } from "react-icons/hi";
+import { useState } from "react";
+import ReviewSmall from "../../components/@layout/reviewContent/ReviewSmall";
+import { AiOutlineShareAlt } from "react-icons/ai";
+import { withTheme } from "styled-components";
+
+const BookDetail = () => {
+  const [bookDetails, setBookDetails] = useState(DummyBookDetail);
+
+  return (
+    <div>
+      {bookDetails.map((bookDetail) => {
+        return (
+          <S.Template key={bookDetail.id}>
+            <S.BookContent>
+              <S.BookContentLeft>
+                <S.ImageDateTemplate>
+                  <S.Image src={bookDetail.imageURL} />
+                  <S.Date>{bookDetail.publishedDate}</S.Date>
+                </S.ImageDateTemplate>
+              </S.BookContentLeft>
+              <S.BookContentCenter>
+                <S.BookIntroudce>
+                  <S.BookCategory>{bookDetail.category}</S.BookCategory>
+                  <S.BookTitleAuthor>
+                    <S.BookTitleAuthorTemplate>
+                      <S.BookTitle>{bookDetail.title}</S.BookTitle>
+                      <S.BookAuthor>{bookDetail.author}</S.BookAuthor>
+                    </S.BookTitleAuthorTemplate>
+                    <S.BookShare>
+                      <AiOutlineShareAlt />
+                    </S.BookShare>
+                  </S.BookTitleAuthor>
+                  <S.BookPrice>{bookDetail.price}</S.BookPrice>
+                  <S.BookExplain>{bookDetail.content}</S.BookExplain>
+                  <S.BookButton>
+                    <Button
+                      text='같이 이야기하기'
+                      width='350px'
+                      height='50px'
+                    />
+                  </S.BookButton>
+                </S.BookIntroudce>
+              </S.BookContentCenter>
+            </S.BookContent>
+            <S.ReviewContent>
+              <S.ReviewContentTemplate>
+                <S.Review>리뷰 달기</S.Review>
+                <S.ReviewClick>
+                  <S.ReviewiIconTemplate>
+                    <S.ReviewIcon>
+                      <HiOutlineChat />
+                    </S.ReviewIcon>
+                    <S.ReviewCount>리뷰 2 +</S.ReviewCount>
+                  </S.ReviewiIconTemplate>
+                  <S.ReviewMore>더보기</S.ReviewMore>
+                </S.ReviewClick>
+                <S.ReviewComponentTemplate>
+                  <ReviewSmall />
+                </S.ReviewComponentTemplate>
+              </S.ReviewContentTemplate>
+            </S.ReviewContent>
+          </S.Template>
+        );
+      })}
+    </div>
+  );
+};
+export default BookDetail;
