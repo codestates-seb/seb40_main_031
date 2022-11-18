1be338182bdbc1762de766851b5059488b7f88bf
rosenfence <rosenfence@gmail.com>
2022-11-18 22:21:26 +0900

    feat: create Main Carousel

---
+++ client/src/components/main/MainCarousel.js
@@ -0,0 +1,72 @@
+import React, { useState, useEffect, useRef } from 'react';
+import * as S from './MainCarousel.style';
+
+const Carousel = () => {
+  const images = useRef([
+    {
+      src: 'https://an2-img.amz.wtchn.net/image/v2/QAYGCBozk5Op9FhbtLpuSg.jpg?jwt=ZXlKaGJHY2lPaUpJVXpJMU5pSjkuZXlKdmNIUnpJanBiSW1SZk1qZ3dlRFF3TUhFNE1DSmRMQ0p3SWpvaUwzWXhMM2gwWjJoaWIyWndZM2Q1T0hsd2VEVnRhbU56SW4wLm5mc1FNMnI5Q0VmSGFKUTRkRk5ocm1FRXFEdTBkemNYcE5hYXdUbC1fakk',
+    },
+    {
+      src: 'https://an2-img.amz.wtchn.net/image/v2/fUK_R2MfkYEyPV7rIqE_tA.jpg?jwt=ZXlKaGJHY2lPaUpJVXpJMU5pSjkuZXlKdmNIUnpJanBiSW1SZk1qZ3dlRFF3TUhFNE1DSmRMQ0p3SWpvaUwzWXlMM04wYjNKbEwySnZiMnN2TVRZME5UazVNRGt6T1RnM01qQXdPREExTmlKOS5Xa0Z2aDloRE5kNjJpLS1UaVUyM0NtODMyZVpxU25uYTFiTl9BdHJXdU5F',
+    },
+    {
+      src: 'https://an2-img.amz.wtchn.net/image/v2/1vHVMgi5VwonuTKYmV19fQ.jpg?jwt=ZXlKaGJHY2lPaUpJVXpJMU5pSjkuZXlKdmNIUnpJanBiSW1SZk1qZ3dlRFF3TUhFNE1DSmRMQ0p3SWpvaUwzWXhMM2R5ZVRCNmFXTnZhVEUxZDJseWVHbG5kVzFwSW4wLmRielV6R015V1NBdGY3Qmk5bEhxeThkYTF3RkJOTkFfRnRHSEhNOTBpNkE',
+    },
+  ]);
+
+  const [current, setCurrent] = useState(0);
+  const [style, setStyle] = useState({ marginLeft: `-${current}00%` });
+
+  const imgSize = useRef(images.current.length);
+
+  const moveSlide = (i) => {
+    let nextIndex = current + i;
+
+    if (nextIndex < 0) nextIndex = imgSize.current - 1;
+    else if (nextIndex >= imgSize.current) nextIndex = 0;
+
+    setCurrent(nextIndex);
+  };
+
+  useEffect(() => {
+    setStyle({ marginLeft: `-${current}00%` });
+  }, [current]);
+
+  return (
+    <S.ContainerDiv>
+      <S.SlideDiv>
+        <S.BtnDiv
+          onClick={() => {
+            moveSlide(-1);
+          }}
+        >
+          &lt;
+        </S.BtnDiv>
+        <S.WindowDiv>
+          <S.FlexBoxDiv style={style}>
+            {images.current.map((img, i) => (
+              <S.ImageDiv
+                key={i}
+                style={{ backgroundImage: `url(${img.src})` }}
+              ></S.ImageDiv>
+            ))}
+          </S.FlexBoxDiv>
+        </S.WindowDiv>
+        <S.BtnDiv
+          onClick={() => {
+            moveSlide(1);
+          }}
+        >
+          &gt;
+        </S.BtnDiv>
+      </S.SlideDiv>
+      <S.PositionDiv>
+        {images.current.map((x, i) => (
+          <div key={i} className={i === current ? 'dot current' : 'dot'}></div>
+        ))}
+      </S.PositionDiv>
+    </S.ContainerDiv>
+  );
+};
+
+export default Carousel;
---
+++ client/src/components/main/MainCarousel.style.js
@@ -0,0 +1,73 @@
+import styled from 'styled-components';
+import { Theme } from '../../style/index';
+
+const ContainerDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const SlideDiv = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const BtnDiv = styled.div`
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+  font-size: 3.3rem;
+  color: ${Theme.PRIMARY.GREEN_DARK};
+  padding: 0 10px;
+`;
+
+const WindowDiv = styled.div`
+  width: 350px;
+  height: 250px;
+
+  overflow: hidden;
+`;
+
+const FlexBoxDiv = styled.div`
+  display: flex;
+`;
+
+const PositionDiv = styled.div`
+  margin-top: 15px;
+  display: flex;
+  justify-content: center;
+
+  .dot {
+    background: ${Theme.PRIMARY.GREEN_LIGHT};
+    border-radius: 100%;
+    height: 10px;
+    width: 10px;
+  }
+
+  .dot + .dot {
+    margin-left: 20px;
+  }
+
+  .current {
+    background: ${Theme.PRIMARY.GREEN_DARK};
+  }
+`;
+
+const ImageDiv = styled.div`
+  width: 350px;
+  height: 250px;
+  background-position: 50% 50%;
+  background-size: contain;
+  background-repeat: no-repeat;
+  flex: none;
+`;
+
+export {
+  ContainerDiv,
+  SlideDiv,
+  BtnDiv,
+  WindowDiv,
+  FlexBoxDiv,
+  PositionDiv,
+  ImageDiv,
+};
