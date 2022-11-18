b9a4d9fe8057423205dff86918373eb040881125
nahoyeon <nahy0107@naver.com>
2022-11-18 17:12:42 +0900

    fix : 컴포넌트 수정 완료

--- client/src/components/@layout/reviewContent/ReviewSmall.js
+++ client/src/components/@layout/reviewContent/ReviewSmall.js
@@ -1,36 +1,31 @@
-import React from 'react';
-import * as S from './ReviewSmall.style'
-
-import {FaRegThumbsUp} from 'react-icons/fa'
+import React from "react";
+import * as S from "./ReviewSmall.style";
+import DummyReviewSmallDetails from "../../../dummyData/DummyReviewSmallDetail";
+import { FaRegThumbsUp } from "react-icons/fa";
+import { useState } from "react";
 
 const ReviewSmall = () => {
-    return(
-        <>
-            <S.Template>
-                <S.Content>
-                    이 책은 내 앞의 대상에 대해 익숙하게 명명하고 
-                    분류함으로써 이해의 그물로 포획혀라는 인간의 높은 자리로부터 벗어나 세계를, 
-                    세계의 쉽사리 범주화 될 수 없는 모든 존재들을 물끄러미 그 자체로 바라보게 한다.
-                    이 책은 내 앞의 대상에 대해 익숙하게 명명하고 
-                    분류함으로써 이해의 그물로 포획혀라는 인간의 높은 자리로부터 벗어나 세계를, 
-                    세계의 쉽사리 범주화 될 수 없는 모든 존재들을 물끄러미 그 자체로 바라보게 한다.
-                    이 책은 내 앞의 대상에 대해 익숙하게 명명하고 
-                    분류함으로써 이해의 그물로 포획혀라는 인간의 높은 자리로부터 벗어나 세계를, 
-                    세계의 쉽사리 범주화 될 수 없는 모든 존재들을 물끄러미 그 자체로 바라보게 한다.
-                    이 책은 내 앞의 대상에 대해 익숙하게 명명하고 
-                    분류함으로써 이해의 그물로 포획혀라는 인간의 높은 자리로부터 벗어나 세계를, 
-                    세계의 쉽사리 범주화 될 수 없는 모든 존재들을 물끄러미 그 자체로 바라보게 한다.
-                </S.Content>
-                <S.BottomContent>
-                    <S.GoodIcon>
-                        <FaRegThumbsUp />
-                    </S.GoodIcon>
-                    <S.LeftText>
-                        5
-                    </S.LeftText>
-                </S.BottomContent>
+  const [reviewSmalls, setReviewSmall] = useState(
+    DummyReviewSmallDetails.slice(0, 2),
+  );
+  return (
+    <div>
+      <S.Display>
+        {reviewSmalls.map((reviewSmall) => {
+          return (
+            <S.Template key={reviewSmall.id}>
+              <S.Content>{reviewSmall.content}</S.Content>
+              <S.BottomContent>
+                <S.GoodIcon>
+                  <FaRegThumbsUp />
+                </S.GoodIcon>
+                <S.LeftText>{reviewSmall.vote}</S.LeftText>
+              </S.BottomContent>
             </S.Template>
-        </>
-    )
-}
+          );
+        })}
+      </S.Display>
+    </div>
+  );
+};
 export default ReviewSmall;
