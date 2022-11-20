92f92a35e111c7b6d7611dc0f87a1fad8e742b22
paquantum <enmw34@naver.com>
2022-11-20 19:04:27 +0900

    Feat member CRUD and Fix Error directory and modify files location (#51)
    * [Fix]build gradle
    
    * feat: JWT 로그인 기능
    
    * Feat member CRUD and Fix error domain
    
    * Delete Book domain
    
    Co-authored-by: enh1535 <enh1535@naver.com>

--- server/src/main/java/com/seb40_main_031/ServerApplication.java
+++ server/src/main/java/com/seb40_main_031/ServerApplication.java
@@ -2,12 +2,16 @@ package com.seb40_main_031;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableJpaAuditing
+//@EnableScheduling
 public class ServerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
+
 	}
 
 }
