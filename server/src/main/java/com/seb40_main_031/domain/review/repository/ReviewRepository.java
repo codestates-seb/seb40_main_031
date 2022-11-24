737f808c729e5c30fcaef94f148c5c4230e3718d
paquantum <enmw34@naver.com>
2022-11-24 20:49:04 +0900

    Modify member info and ExceptionCode (#75)

---
+++ server/src/main/java/com/seb40_main_031/domain/review/repository/ReviewRepository.java
@@ -0,0 +1,17 @@
+package com.seb40_main_031.domain.review.repository;
+
+import com.seb40_main_031.domain.review.entity.Review;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+
+@Repository
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+    //      SELECT * FROM REVIEW where book_id = 1
+    Page<Review> findAllByBookBookId(long bookId, Pageable pageable);
+    Page<Review> findAllByMemberId(Long memberId, Pageable pageable);
+}
---
+++ server/src/main/java/com/seb40_main_031/domain/review/service/ReviewService.java
@@ -0,0 +1,70 @@
+package com.seb40_main_031.domain.review.service;
+
+import com.seb40_main_031.domain.review.entity.Review;
+import com.seb40_main_031.domain.review.repository.ReviewRepository;
+import com.seb40_main_031.global.error.exception.BusinessLogicException;
+import com.seb40_main_031.global.error.exception.ExceptionCode;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+import java.util.Optional;
+
+@Service
+public class ReviewService {
+
+    private final ReviewRepository reviewRepository;
+
+    public ReviewService(ReviewRepository reviewRepository) {
+        this.reviewRepository = reviewRepository;
+    }
+
+    // 리뷰 생성
+    public Review createReview(Review review){
+        return reviewRepository.save(review);
+    }
+
+
+    // 리뷰 수정
+    public Review modifiedReview(Review review) {
+        Review findReview = findVerifiedReview(review.getReviewId());
+        findReview.setContent(review.getContent());
+
+        return reviewRepository.save(findReview);
+    }
+
+    // 리뷰 단일 찾기
+    public Review findReview(long reviewId) {
+        Review findReview = findVerifiedReview(reviewId);
+        return findReview;
+    }
+
+    // 리뷰 리스트 찾기
+    public Page<Review> findReviews(long bookId, int page, int size) {
+        Pageable pageReview = PageRequest.of(page,size, Sort.by("reviewId").descending());
+        return reviewRepository.findAllByBookBookId(bookId, pageReview);
+
+    }
+
+    // 리뷰 삭제
+    public void deleteReview(long reviewId, long memberId){
+        Review review = findVerifiedReview(reviewId);
+        if(review.getMemberId() == memberId) reviewRepository.delete(review);
+    }
+
+
+    // 리뷰 여부 검증
+    private Review findVerifiedReview(long reviewId){
+        Optional<Review> optionalReview =
+                reviewRepository.findById(reviewId);
+        Review findReview = optionalReview.orElseThrow(()->
+                new BusinessLogicException(ExceptionCode.REVIEW_NOT_FOUND));
+
+        return findReview;
+    }
+
+}
