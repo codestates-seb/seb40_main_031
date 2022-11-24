b17ed76c3672d6aeab663b7c27ffe769aba8a44e
joewontae <tyuiop3762@naver.com>
2022-11-24 15:24:00 +0900

    ✒️Feat: 모달컴포넌트 구현

---
+++ client/src/components/@common/modal/Modal.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Wrap } from './Modal.style';
+
+const Modal = () => {
+  return <Wrap>모달창입니다</Wrap>;
+};
+
+export default Modal;
---
+++ client/src/components/@common/modal/Modal.style.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+const Wrap = styled.div`
+  width: 30vw;
+  height: 10vh;
+  background: red;
+`;
+export { Wrap };
