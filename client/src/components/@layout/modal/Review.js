3f5ab1966e78f807563d9c121ff1cc781b161ff1
나호연 <91938347+nahoyeon@users.noreply.github.com>
2022-11-26 22:30:48 +0900

    fix : review modal 오류 해결 및 css 수정(반응형 추가) (#97)
    머지 했습니다

--- client/src/components/@layout/modal/Review.js
+++ client/src/components/@layout/modal/Review.js
@@ -10,44 +10,41 @@ import {
 } from 'components/@layout/modal/Review.style';
 import { IoCloseOutline } from 'react-icons/io5';
 import { Button } from 'components';
-import { useState, useRef } from 'react';
+import { useRef } from 'react';
 
-const ModalReview = () => {
-  const [closeModal, setCloseModal] = useState(true);
+const ModalReview = ({ setModal }) => {
   const outside = useRef();
 
   const modalHandlered = () => {
-    setCloseModal(!closeModal);
+    setModal(false);
   };
 
   return (
     <div>
-      {closeModal === true ? (
-        <ModalBackground
-          ref={outside}
-          onClick={(e) => {
-            if (e.target === outside.current) setCloseModal(false);
-          }}
-        >
-          <ContainerDiv>
-            <HeaderDiv>
-              <TitleSpan>책제목</TitleSpan>
-              <CloseIconSpan>
-                <IoCloseOutline size='45' onClick={modalHandlered} />
-              </CloseIconSpan>
-            </HeaderDiv>
+      <ModalBackground
+        ref={outside}
+        onClick={(e) => {
+          if (e.target === outside.current) setModal(false);
+        }}
+      >
+        <ContainerDiv>
+          <HeaderDiv>
+            <TitleSpan>책제목</TitleSpan>
+            <CloseIconSpan>
+              <IoCloseOutline size='45' onClick={modalHandlered} />
+            </CloseIconSpan>
+          </HeaderDiv>
 
-            <ReviewTextarea
-              type='text'
-              placeholder='이 작품에 대한 생각을 자유롭게 표현해주세요.'
-            />
+          <ReviewTextarea
+            type='text'
+            placeholder='이 작품에 대한 생각을 자유롭게 표현해주세요.'
+          />
 
-            <FooterDiv>
-              <Button text='작성' width='200px' height='60px' />
-            </FooterDiv>
-          </ContainerDiv>
-        </ModalBackground>
-      ) : null}
+          <FooterDiv>
+            <Button text='작성' width='200px' height='60px' />
+          </FooterDiv>
+        </ContainerDiv>
+      </ModalBackground>
     </div>
   );
 };
