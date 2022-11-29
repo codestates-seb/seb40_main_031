74b07d613962ed51a0fd927251f3b890f4e0f030
rosenfence <rosenfence@gmail.com>
2022-11-30 06:08:48 +0900

    design: 모달창 버튼 색상 Theme에 맞게 변경

--- client/src/components/alertModal/AlertModal.style.js
+++ client/src/components/alertModal/AlertModal.style.js
@@ -4,6 +4,11 @@ import { Button } from 'react-bootstrap';
 
 const CloseButton = styled(Button)`
   background-color: ${Theme.PRIMARY.GREEN};
+  border: none;
+
+  &:hover {
+    background-color: ${Theme.PRIMARY.GREEN_DARK};
+  }
 `;
 
 export { CloseButton };
